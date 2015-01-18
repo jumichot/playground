12f59733f6149fd59fd422d9a5f08d26a652d335
julien michot <ju.michot@gmail.com>
2015-01-18 23:36:28 +0100

    Update script for casino

--- casper_test/example.js
+++ casper_test/example.js
@@ -1,10 +1,10 @@
 var casper = require('casper').create();
-// this.echo(this.getHTML('.search-zipcode form'));
-// var casper = require('casper').create({ verbose: true, logLevel: "debug" });
 
+// Login on the homepage
 casper.start('http://www.monsieurdrive.com/', function(response) {
+    console.log('Login on the homepage');
+    // get title of the page
     this.echo(this.getTitle());
-    // require('utils').dump(response);
     this.fillSelectors('.search-zipcode form', {
       'input[name="user[formatted_address]"]':  'Issy les Moulineaux',
       'input[name="user[latitude]"]':           '48.8245306',
@@ -12,6 +12,7 @@ casper.start('http://www.monsieurdrive.com/', function(response) {
     }, true);
 });
 
+// Add a product
 casper.thenOpen('http://www.monsieurdrive.com/rayons/category-boissons/category-eaux', function() {
   casper.waitForSelector('.addtocart', function success() {
       casper.click('.addtocart');
@@ -22,6 +23,7 @@ casper.thenOpen('http://www.monsieurdrive.com/rayons/category-boissons/category-
   );
 });
 
+// Go to cart
 casper.then(function(){
   casper.waitForSelector('#checkout', function success() {
       casper.click('#checkout');
@@ -32,6 +34,7 @@ casper.then(function(){
   );
 });
 
+// Validate cart
 casper.then(function(){
   casper.waitForSelector('.btn-checkout', function success() {
     this.click('.btn-checkout');
@@ -45,6 +48,7 @@ casper.then(function(){
   );
 });
 
+// sign in in the modal
 casper.then(function(){
   casper.waitForSelector('form[action="/users/sign_in"]', function success() {
       casper.evaluate(function () {
@@ -59,15 +63,21 @@ casper.then(function(){
   );
 });
 
+// submit the modal
 casper.then(function(){
-  this.wait(2000, function() {
-    this.click('.js-cp_modal_submit_link');
-  });
-  this.wait(2000, function() {
+  casper.waitForSelector('.js-cp_modal_submit_link', function success() {
+      this.click('.js-cp_modal_submit_link');
+    },
+    function fail() {
+      test.assertExists('.js-cp_modal_submit_link');
+    }
+  );
+  this.wait(1000, function() {
     this.capture('step2_sign_in.png');
   });
 });
 
+// launch the checkout
 casper.then(function(){
   casper.waitForSelector('.btn-checkout', function success() {
     this.click('.btn-checkout');
@@ -76,7 +86,7 @@ casper.then(function(){
       test.assertExists(".btn-checkout");
     }
   );
-  this.wait(2000, function() {
+  this.wait(1000, function() {
     this.capture('step3_checkout.png');
   });
 });
@@ -94,6 +104,7 @@ casper.then(function(){
   });
 });
 
+// check if command finalized
 casper.then(function(){
   casper.waitForSelector('.js-checkout_status .icon-ok', function success() {
     this.click('.js-checkout_tranfer_btn');
@@ -102,9 +113,38 @@ casper.then(function(){
       test.assertExists('.js-checkout_status .icon-ok');
     }
   );
-  this.wait(2000, function() {
+  this.wait(5000, function() {
     this.capture('step5_finnish.png');
   });
 });
 
+casper.then(function(){
+  casper.waitForPopup(/casinodrive/, function() {
+    this.wait(1000, function() {
+      this.capture('step6_finnish.png');
+    });
+  });
+  // IN POPUP
+  casper.withPopup(/casinodrive/, function() {
+    console.log("in popup:")
+    this.clickLabel('Me connecter / Créer mon compte', 'a');
+
+    casper.then(function(){
+      this.wait(1000, function() {
+        console.log("wait #id")
+        casper.waitForSelector('#identifiant-id', function success() {
+          casper.evaluate(function () {
+            $("#identifiant-id").val("clebruit@yahoo.fr")
+            $("#secure-id").val("clebruit@yahoo.fr")
+          });
+          this.clickLabel('Me connecter', 'button');
+        });
+      });
+    });
+
+    this.wait(1000, function() {
+      this.capture('step7_target_shop.png');
+    });
+  });
+});
 casper.run();
