15af4326ae84d697339b8591b940f75a0509dcdb
julien michot <ju.michot@gmail.com>
2015-01-19 14:23:38 +0100

    Fonctional poc

--- casper_test/example.js
+++ casper_test/example.js
@@ -1,7 +1,9 @@
 log_step = function(msg) {
   log_step.step = log_step.step || 0;
   log_step.step = log_step.step + 1;
+  console.log("====================================");
   console.log("STEP " + log_step.step + " " + msg);
+  console.log("====================================");
   casper.capture('step' + log_step.step + '_'+ msg.replace(/\s+/g, '_') +'.png');
 }
 
@@ -13,137 +15,80 @@ var casper = require('casper').create({
 
 // Login on the homepage
 casper.start('http://www.monsieurdrive.com/', function(response) {
-    log_step("login on the homepage");
+    log_step("fill zipcode form on the homepage");
     this.fillSelectors('.search-zipcode form', {
       'input[name="user[formatted_address]"]':  'Nevers, France',
       'input[name="user[latitude]"]':           '46.99089600000001',
       'input[name="user[longitude]"]':          '3.162845000000061'
     }, true);
+    // casper.click('.js-cp_modal_submit_link');
 });
 
-// Add a product
-casper.thenOpen('http://www.monsieurdrive.com', function() {
-    this.wait(1000, function() {
-      console.log('step 01 : addtocart');
-      this.capture('step01_addtocart.png');
-    });
-  casper.waitForSelector('.addtocart', function success() {
-      casper.click('.addtocart');
-    },
-    function fail() {
-      test.assertExists("fail to find .addtocart");
-    }
-  );
-});
-
-// Go to cart
-casper.then(function(){
-  casper.waitForSelector('#checkout', function success() {
-      casper.click('#checkout');
-    },
-    function fail() {
-      test.assertExists("fail to find #checkout");
-    }
-  );
-});
-
-// Validate cart
-casper.then(function(){
-  casper.waitForSelector('.btn-checkout', function success() {
-    this.click('.btn-checkout');
-    this.wait(1000, function() {
-      this.capture('step1_checkout.png');
-    });
-    },
-    function fail() {
-      test.assertExists(".btn-checkout");
-    }
-  );
-});
-
-// sign in in the modal
-casper.then(function(){
-  casper.waitForSelector('form[action="/users/sign_in"]', function success() {
+casper.then( function() {
+    casper.waitForSelector('form[action="/users/sign_in"]', function success() {
+      log_step("sign in with the modal");
       casper.evaluate(function () {
         $('.show-form-link').first().click()
         $("form[action='/users/sign_in'] #user_email").val("julien.michot@monsieurdrive.com")
         $("form[action='/users/sign_in'] #user_password").val("123456")
       });
-    },
-    function fail() {
-      test.assertExists("form[action='/users/sign_in']");
-    }
-  );
+    });
 });
 
-// submit the modal
 casper.then(function(){
   casper.waitForSelector('.js-cp_modal_submit_link', function success() {
       this.click('.js-cp_modal_submit_link');
-    },
-    function fail() {
-      test.assertExists('.js-cp_modal_submit_link');
-    }
-  );
-  this.wait(1000, function() {
-    this.capture('step2_sign_in.png');
+      log_step("submit the signin modal");
+    });
+});
+
+casper.then(function() {
+  casper.waitForSelector('.addtocart', function success() {
+    log_step("add a product to the cart");
+    casper.click('.addtocart[data-product-id="521d2472b30f7cad8032fdc0"]');
   });
 });
 
-// launch the checkout
+casper.then(function(){
+  casper.waitForSelector('#checkout', function success() {
+    casper.click('#checkout');
+    log_step("go to cart page");
+  });
+});
+
 casper.then(function(){
   casper.waitForSelector('.btn-checkout', function success() {
     this.click('.btn-checkout');
-    },
-    function fail() {
-      test.assertExists(".btn-checkout");
-    }
-  );
-  this.wait(1000, function() {
-    this.capture('step3_checkout.png');
+    log_step("launch checkout commander btn");
   });
 });
 
 casper.then(function(){
-  casper.waitForSelector('.account_btn', function success() {
-    this.click('.account_btn');
-    },
-    function fail() {
-      test.assertExists(".account_btn");
-    }
-  );
-  this.wait(3000, function() {
-    this.capture('step4_running_checkout.png');
+  casper.waitForSelector('.account_btn ', function success() {
+    this.click('.account_btn ');
+    log_step("transfert cart");
   });
 });
 
-// check if command finalized
 casper.then(function(){
   casper.waitForSelector('.js-checkout_status .icon-ok', function success() {
     this.click('.js-checkout_tranfer_btn');
-    },
-    function fail() {
-      test.assertExists('.js-checkout_status .icon-ok');
-    }
-  );
-  this.wait(5000, function() {
-    this.capture('step5_finnish.png');
+    log_step("checkout finish");
   });
 });
 
 casper.then(function(){
   casper.waitForPopup(/carrefour/, function() {
-    this.wait(1000, function() {
-      this.capture('step6_finnish.png');
-    });
+    log_step("merchant shop");
   });
-  // IN POPUP
+
   casper.withPopup(/carrefour/, function() {
-    console.log("in popup:")
+    log_step("display shop merchant");
 
     this.wait(1000, function() {
-      this.capture('step7_target_shop.png');
+      log_step("display shop merchant");
     });
   });
 });
+
 casper.run();
