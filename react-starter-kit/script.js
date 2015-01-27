63678e2b6b7d688e76b55f3482d98d19a19f0441
julien michot <ju.michot@gmail.com>
2015-01-27 14:16:47 +0100

    Add mardown transformation

--- react-starter-kit/script.js
+++ react-starter-kit/script.js
@@ -1,11 +1,15 @@
+var converter = new Showdown.converter();
+
 var Comment = React.createClass({
+
   render: function(){
+    var rawHtml = converter.makeHtml(this.props.children)
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawHtml }}></span>
       </div>
       );
   }
