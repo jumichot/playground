5562f468136c9bedc1dadfe9436dd2ae315e2c9e
julien michot <ju.michot@gmail.com>
2015-02-21 11:06:45 +0100

    Update projects

--- react-tweets/config.js
+++ react-tweets/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'put-yours-here',
-    consumer_secret: 'put-yours-here',
-    access_token_key: 'put-yours-here',
-    access_token_secret: 'put-yours-here'
+    consumer_key: 'ELpGZ1DbIyEkkTUViwxk9pCXB',
+    consumer_secret: 'fud0kiatazC9QMONvpXVA9ClLyUpUuDXhH1Tn6DruHDgy3VrKo',
+    access_token_key: '253526321-QoOFej26bboF2NGUz8V9LTOlGNTp1s8g7fPWlqxk',
+    access_token_secret: 'dXik2qzPfnUadYvYWMx6LMWMZxUCGMIR7nAEfHAfuDc1z'
   }
 }
