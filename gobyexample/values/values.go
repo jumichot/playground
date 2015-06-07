c5a86620c6e01d001e27410ced6d4b2f651ff143
julien michot <ju.michot@gmail.com>
2015-06-07 12:25:35 +0200

    Add value example

---
+++ gobyexample/values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("lol" + " lol")
+	fmt.Println("integer 1+1", 1+1)
+	fmt.Println("float", 2/8.0)
+	fmt.Println("false", true && false)
+	fmt.Println("false", false && false)
+	fmt.Println("true", false || true)
+	fmt.Println("false", false || false)
+	fmt.Println("true", !false)
+}
